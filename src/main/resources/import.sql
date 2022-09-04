22ac9f369191f166340409deec75e609ad1d136b
Fabrício Takaki <fabriciohtakaki@gmail.com>
2022-09-04 18:31:09 -0300

    persisting data to db

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into fruta(id, nome, qtd) values(nextval('hibernate_sequence'), "morango", 5);
+insert into fruta(id, nome, qtd) values(nextval('hibernate_sequence'), "banana", 1);
+insert into fruta(id, nome, qtd) values(nextval('hibernate_sequence'), "laranja", 7);
